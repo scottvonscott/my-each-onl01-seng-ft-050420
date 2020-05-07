a1a43836ad240336e4926065383ede32240022e4
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 21:21:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,22 +1,21 @@
+
+
+
+
+
+
 def my_each(collection)
-
+  if block_given?
+    i = 0
+     while i < collection.length
+       yield collection[i]
+       i += 1
+     end
+     collection
+   else
+     puts "Hey! No block was given!"
 end
-
-
-
-
-
-# def my_each(collection)
-#   if block_given?
-#     i = 0
-#      while i < collection.length
-#        yield collection[i]
-#        i += 1
-#      end
-#    else
-#      collection
-# end
-# end
+end
 
 
 # def hello_t(array)
