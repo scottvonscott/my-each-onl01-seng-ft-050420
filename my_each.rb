5baca4018727201a2a4eaf53268990e144d1e2ce
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 21:27:32 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(collection)
+  if block_given?
+    i = 0
+     while i < collection.length
+       yield collection[i]
+       i += 1
+     end
+     collection
+   else
+      "Hey! No block was given!"
+end
 end
